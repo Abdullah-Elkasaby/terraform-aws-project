47f1a0dfc8456407bc4479cc144ded36ac7a1508
Abdullah <abdullah-elkasaby@users.noreply.github.com>
2023-05-23 17:40:50 +0300

    added backend

---
+++ terraform-aws-providers.tf
@@ -0,0 +1,19 @@
+terraform {
+ 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.49.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
